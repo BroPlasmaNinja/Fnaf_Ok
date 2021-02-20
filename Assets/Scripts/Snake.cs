8df46b22b11d6e07d73d4b84b44eefbb80e7601f
BroPlasmaNinja <HeadMenlvl100@yandex.ru>
2021-02-20 19:37:37 +0400

    ы

---
+++ Assets/Scripts/Snake.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Snake : MonoBehaviour
+{
+    [SerializeField] private GameObject PointArray;
+    private NavMeshAgent agent;
+    [SerializeField] private float timer = 60;
+    private float tim = 0f;
+    [SerializeField] private bool MoveInStart = false;
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        tim = timer;
+
+        if (MoveInStart)
+        {
+            int a = Random.Range(0, PointArray.transform.childCount);
+            agent.SetDestination(PointArray.transform.GetChild(a).GetChild(Random.Range(0, PointArray.transform.GetChild(a).childCount)).position);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (tim<0)
+        {
+            int a = Random.Range(0, PointArray.transform.childCount);
+            agent.SetDestination(PointArray.transform.GetChild(a).GetChild(Random.Range(0,PointArray.transform.GetChild(a).childCount)).position);
+            tim = timer;
+        }
+        tim -= Time.deltaTime;
+    }
+}
