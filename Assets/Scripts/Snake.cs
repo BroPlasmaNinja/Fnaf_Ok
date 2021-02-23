6d7da10e816a7d85cec9b66bf6875baa2e8a7e2c
BroPlasmaNinja <HeadMenlvl100@yandex.ru>
2021-02-23 13:24:13 +0400

    *.bat update

--- Assets/Scripts/Snake.cs
+++ Assets/Scripts/Snake.cs
@@ -7,25 +7,14 @@ public class Snake : MonoBehaviour
 {
     [SerializeField] private GameObject PointArray;
     private NavMeshAgent agent;
-    [SerializeField] private bool MoveInStart = false;
     [SerializeField] [Range(0,3600)] private int Timer = 5;
-    [SerializeField] [Range(0, 3600)] private int StayOnStart = 0;
     private float tim;
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
-
-
-        if (MoveInStart)
-        {
-            int a = Random.Range(0, PointArray.transform.childCount);
-            agent.SetDestination(PointArray.transform.GetChild(a).GetChild(Random.Range(0, PointArray.transform.GetChild(a).childCount)).position);
-            tim = Timer;
-        }
-        else
-        {
-            tim = StayOnStart;
-        }
+        int a = Random.Range(0, PointArray.transform.childCount);
+        agent.SetDestination(PointArray.transform.GetChild(a).GetChild(Random.Range(0, PointArray.transform.GetChild(a).childCount)).position);
+        tim = Timer;
     }
 
     void Update()
